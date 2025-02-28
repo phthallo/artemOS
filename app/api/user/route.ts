3a5bdabab7d57c40611cf4d64f4485aaab7cc2ad
aqua <84078890+phthallo@users.noreply.github.com>
2025-02-27 20:16:54 -0500

    fix: case where no wakatime data found

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -13,27 +13,37 @@ export async function GET() {
             {filterByFormula: `AND({Email} = "${session?.user.email}", IF({Created} > DATETIME_PARSE('2025-01-01', 'YYYY-MM-DD'), TRUE(), FALSE()))`,
         fields: ["Playable URL", "Code URL", "Screenshot", "YSWS"]}
         ).all()))
+        const sortedProjects =  projects.map((project) => {
+            return  { 
+                "Playable URL": project["fields"]["Playable URL"] ? project["fields"]["Playable URL"] : null ,
+                "Code URL": project["fields"]["Code URL"] ? project["fields"]["Code URL"] : null,
+                "Screenshot": project["fields"]["Screenshot"] ? project["fields"]["Screenshot"][0]["url"] : null,
+                "YSWS": project["fields"]["YSWS"] ? project["fields"]["YSWS"] : null
+            }
+        })
         const wakaTime = (await fetch(`https://waka.hackclub.com/api/compat/wakatime/v1/users/${session?.slack_id}/stats/year?category=coding`))
-        const wakaTimeBody = await (wakaTime).json()
         if (wakaTime.status === 200){
+            const wakaTimeBody = await (wakaTime).json()
             const response = {
-                projects: projects.map((project) => {
-                    return  { 
-                        "Playable URL": project["fields"]["Playable URL"] ? project["fields"]["Playable URL"] : null ,
-                        "Code URL": project["fields"]["Code URL"] ? project["fields"]["Code URL"] : null,
-                        "Screenshot": project["fields"]["Screenshot"] ? project["fields"]["Screenshot"][0]["url"] : null,
-                        "YSWS": project["fields"]["YSWS"] ? project["fields"]["YSWS"] : null
-                    }
-                }), 
+                projects: sortedProjects, 
                 wakatime: {
                     total_seconds: (wakaTimeBody)["data"]["total_seconds"], 
                     human_readable_total: (wakaTimeBody)["data"]["human_readable_total"]}}
 
             return NextResponse.json(response)
-        } else {
+        } else if (wakaTime.status === 403) { // no wakatime data on hand
+            const response = {
+                projects: sortedProjects, 
+                wakatime: {
+                    total_seconds: 0, 
+                    human_readable_total: "0 hrs 0 mins"
+                }
+            }
+            return NextResponse.json(response)    
+        } {
             return NextResponse.json({error: "Something went wrong", status: 400})
         }
     } else {
-        return NextResponse.json({error: "not authed", status: 402})
+        return NextResponse.json({error: "Not authed", status: 403})
     }
 }
